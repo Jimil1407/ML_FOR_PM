bfcf876f8a1c96e8f47d2495e5323869d3a26976
Jimil Digaswala <92706888+Jimil1407@users.noreply.github.com>
2023-06-24 21:20:24 +0530

    Add files via upload

---
+++ host_website.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+import sklearn
+from nltk.stem.porter import PorterStemmer
+#nltk.download("punkt")
+#nltk.download("stopwords")
+ps = PorterStemmer()
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    text = y[:]
+    y.clear()
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation: y.append(i)
+    text = y[:]
+    y.clear()
+    for i in text:
+        y.append(ps.stem(i))
+    return " ".join(y)
+tfidf = pickle.load(open('/Users/Anushka/Desktop/vectorizer.pkl','rb'))
+#Path to Vector model file
+model = pickle.load(open('/Users/Anushka/Desktop/model.pkl','rb'))
+#Path to Multinomial Naives Bayes Model
+st.title("SMS Spam Classifier")
+input_sms = st.text_area("Enter the message")
+if st.button('Predict'):
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
